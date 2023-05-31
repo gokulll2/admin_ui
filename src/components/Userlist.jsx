b7222db5d7121174877b916928804da41ab208a7
Gokul <gokulsethi69@gmail.com>
2023-05-31 23:09:01 +0530

    main

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Userlist = () => {
     const[users,setUsers] = React.useState([]);
+    const[edit,setEdit]=React.useState(false);
+    const[edituserID,seteditUser]=React.useState(null)
 
     React.useEffect(() => {
         fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
@@ -13,17 +15,24 @@ const Userlist = () => {
         console.log('Checkbox for user with ID ${userId} changed')
     }
     const handleEditUser= (userID) => {
-        console.log('Checkbox for user with ID ${userId} changed')
+        setEdit(true)
+        seteditUser(userID)
     }
     const handleDeleteUser = (userID) => {
-
+       const updatedUsers=users.filter((user) => user.id!==userID)
+       setUsers(updatedUsers)
     }
+    
   return (
     <div className='container'>
         <table>
 
             <thead>
                 <tr>
+                    <th colspan='5'> <input className='p' type='text' placeholder='Search'/></th>
+                </tr>
+                <tr>
+                    <th></th>
                     <th>Id</th>
                     <th>Name</th>
                     <th>Email</th>
@@ -34,12 +43,18 @@ const Userlist = () => {
             <tbody>
                 {
                     users.map((user) => (
+                        
                         <tr key={user.id}>
+                            <td><input onChange={() => handleCheckBox(user.id)} type='checkbox'/></td>
                             <td>
                                 {user.id}
                             </td>
                             <td>
-                                {user.name}
+                                {edit && edituserID==user.id?
+                                <input type='text'/>
+                                :
+                                user.name
+                                }
                             </td>
                             <td>
                                 {user.email}
@@ -48,7 +63,10 @@ const Userlist = () => {
                                 {user.role}
                             </td>
                             <td>
-
+                                <button onClick={() => handleEditUser(user.id)}> EDIT </button>
+                                <button onClick={() => handleDeleteUser(user.id)}>
+                                Delete
+                                </button>
                             </td>
                         </tr>
                         ))
